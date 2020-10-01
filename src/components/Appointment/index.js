f3d2739e3537272897e0e0281bc7f68b92506caf
zainab66 <zainab66r@gmail.com>
2020-09-30 23:34:06 -0400

    displayed appointment

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,11 +2,17 @@ import React from "react";
 
 
 import "./styles.scss";
+import Header from "./Header";
+import Empty from "./Empty";
+import Show from "./Show";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <article className="appointment">
+    <Header time={props.time} />
+    {props.interview? <Show student={props.interview.student} interviewer={props.interview.interviewer} /> : <Empty />}
+  </article>
   );
 }
 
