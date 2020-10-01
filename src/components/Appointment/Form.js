008f01acf428aea0a5907bc8732ce4a6b57f617d
zainab66 <zainab66r@gmail.com>
2020-09-30 23:24:15 -0400

    state added to Appointment Form

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState}  from 'react';
 
 import InterviewerList from '../InterviewerList';
 import Button from '../Button';
@@ -7,28 +7,45 @@ import { action } from '@storybook/addon-actions/dist/preview';
 
 
 export default function Form(props) {
+    const [name, setName] = useState(props.name || "");
+    const [interviewer, setInterviewer] = useState(props.interviewer || null);
+    const reset = () => {
+      setName("");
+      setInterviewer(null);
+    };
+    const cancel = () => {
+      reset();
+      props.onCancel();
+    }
+  
+    const validate = () => {
+  
+      props.onSave(name, interviewer);
+    };
 
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+      <form autoComplete="off" onSubmit={event => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
+            value={name}
+            onChange={e => {
+              setName(e.target.value);
+            }}
             /*
               This must be a controlled component
             */
           />
         </form>
-        <InterviewerList interviewers={props.interviewers} value={props.interviewer} onChange={action("setInterviewer")} />
-      </section>
+        <InterviewerList interviewers={props.interviewers} value={interviewer} onChange={setInterviewer} />      </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button danger onClick={props.onCancel} >Cancel</Button>
-          <Button confirm onClick={props.onSave} >Save</Button>
+          <Button danger onClick={cancel} >Cancel</Button>
+          <Button confirm onClick={validate} >Save</Button>
         </section>
       </section>
     </main>
